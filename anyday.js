be698b0af8910d4eb6c757ecbf8410132f319587
Kunal Mandalia <kunal.v.mandalia@gmail.com>
2022-03-14 22:06:25 +0000

    feat: close help box onload

--- anyday.js
+++ anyday.js
@@ -35,8 +35,36 @@ wordleAnyday.getGameDate = function () {
   return Date.parse(new Date())
 }
 
+wordleAnyday.afterward = function () {
+  console.log('wordleAnyday.attempts += 1')
+  // run after next tick (or after js files are loaded)
+  window.setTimeout(afterLoad, 10)
+  function afterLoad() {
+    try {
+      // hide helper popup
+      var gameApp = document.querySelector('body > game-app')
+      if (!gameApp) {
+        return wordleAnyday.afterward()
+      }
+
+      var closeIcon = gameApp.shadowRoot
+        .querySelector('#game > game-modal')
+        .shadowRoot.querySelector('div > div > div > game-icon')
+
+      if (!closeIcon) {
+        wordleAnyday.afterward()
+      } else {
+        closeIcon.click()
+      }
+    } catch (e) {
+      console.error(e)
+    }
+  }
+}
+
 wordleAnyday.main = function () {
   wordleAnyday.setInitialDate()
+  wordleAnyday.afterward()
 }
 
 wordleAnyday.main()
